ede2bc2e31a9f8ac0bc04d2301a0233c1eed097c
Lginn26 <lginn26@users.noreply.github.com>
2018-04-17 16:13:03 -0400

    Added bump switch, limit switch and motar functionality in motorSwitchReverse.c
    Added bump switch, limit switch, and motor functionality in
    motorSwitchTwoHalfs.c

---
+++ motorSwitchTwoHalfs.c
@@ -0,0 +1,28 @@
+#pragma config(Sensor, in1,    pwrExp,         sensorAnalog)
+#pragma config(Sensor, dgtl1,  quad,           sensorQuadEncoder)
+#pragma config(Sensor, dgtl3,  limit,          sensorTouch)
+#pragma config(Sensor, dgtl4,  bump,           sensorTouch)
+#pragma config(Sensor, dgtl5,  ledgreen,       sensorLEDtoVCC)
+#pragma config(Sensor, dgtl6,  ledyellow,      sensorLEDtoVCC)
+#pragma config(Sensor, dgtl7,  ledred,         sensorLEDtoVCC)
+#pragma config(Sensor, dgtl8,  jumper,         sensorTouch)
+#pragma config(Motor,  port5,           drive1,        tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port6,           drive2,        tmotorVex393_MC29, openLoop, reversed)
+
+task main() {
+	while (true) {
+		if (SensorValue[bump] == true || SensorValue[limit] == true) {
+			motor[drive1] = 63;
+			motor[drive2] = 63;
+		}
+		if (SensorValue[bump] == true && SensorValue[limit] == true) {
+			motor[drive1] = 127;
+			motor[drive2] = 127;
+		}
+		else {
+			motor[drive1] = 0;
+			motor[drive2] = 0;
+	  }
+	}
+
+}
